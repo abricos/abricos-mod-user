ef6653b2006ae7561be5d2f2fb590f479822e507
Alexander Kuzmin <roosit@abricos.org>
2014-03-13 22:49:51 +0400

    workfix

--- src/js/lib.js
+++ src/js/lib.js
@@ -28,7 +28,8 @@ Component.entryPoint = function(NS){
                 'savedata': login.toJSON()
             }, function(err, r){
                 console.log(arguments);
-                // NS.life(callback);
+                console.log(this);
+
             });
         }
     };
@@ -55,56 +56,4 @@ Component.entryPoint = function(NS){
         callback(null, NS.appInstance);
     };
 
-/*
-    NS.lif = function(f){
-        return L.isFunction(f) ? f : function(){
-        };
-    };
-    NS.life = function(f, p1, p2, p3, p4, p5, p6, p7){
-        f = NS.lif(f);
-        f(p1, p2, p3, p4, p5, p6, p7);
-    };
-
-
-    var Manager = function(callback){
-        this.init(callback);
-    };
-    Manager.prototype = {
-        init: function(callback){
-            NS.manager = this;
-            NS.life(callback);
-        },
-        ajax: function(d, callback){
-            d = d || {};
-            d['tm'] = Math.round((new Date().getTime()) / 1000);
-
-            Brick.ajax('{C#MODNAME}', {
-                'data': d,
-                'event': function(request){
-                    var d = L.isValue(request) && L.isValue(request.data) ? request.data : null,
-                        result = L.isValue(d) ? (d.result ? d.result : null) : null;
-
-                    NS.life(callback, result);
-                }
-            });
-        },
-        login: function(login, callback){
-            this.ajax({
-                'do': 'login',
-                'savedata': login.toJSON()
-            }, function(r){
-                NS.life(callback);
-            });
-        }
-    };
-    NS.Manager = Manager;
-
-    NS.manager = null;
-    NS.initManager = function(callback){
-        if (L.isValue(NS.manager)){
-            return callback(NS.manager);
-        }
-        NS.manager = new NS.Manager(callback);
-    };
-    /**/
 };
