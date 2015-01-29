b7d554e72c0151007073a1828a3659d7ed1a52ad
Alexander Kuzmin <roosit@abricos.org>
2015-01-29 18:22:17 +0300

    add TestAPI

---
+++ test/api/testAPI.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var Abricos = require('abricos-rest');
+var helper = Abricos.helper;
+var should = require('should');
+
+describe('User Test API functions', function(){
+
+    var userModule;
+
+    beforeEach(function(done){
+        var api = new Abricos.API();
+        userModule = api.getModule('user');
+
+        done();
+    });
+
+    describe('User Module API ', function(){
+
+        var testAPI;
+
+        it('testAPI instance', function(done){
+            testAPI = userModule.testAPI();
+            should.exist(testAPI);
+
+            done();
+        });
+
+        it('registerUser()', function(done){
+            testAPI.registerUser(function(err, user){
+                should.not.exist(err);
+                should.exist(user);
+                done();
+            });
+        });
+
+    });
+});
