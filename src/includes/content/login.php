c7de2cc519d0e35fa131f9736b7c8985c7926310
Alexander Kuzmin <roosit@abricos.org>
2017-01-31 15:14:31 +0300

    bugfix: auth script without js

--- src/includes/content/login.php
+++ src/includes/content/login.php
@@ -7,23 +7,49 @@
  * @author Alexander Kuzmin <roosit@abricos.org>
  */
 
-
 $brick = Brick::$builder->brick;
-$userMod = Abricos::$user;
-$userManager = $userMod->GetManager();
+$v = $brick->param->var;
 
-$p_login = Abricos::CleanGPC('p', 'login', TYPE_STR);
-$p_pass = Abricos::CleanGPC('p', 'password', TYPE_STR);
+$p_login = Abricos::CleanGPC('p', 'username', TYPE_STR);
+$p_password = Abricos::CleanGPC('p', 'password', TYPE_STR);
+$p_autologin = Abricos::CleanGPC('p', 'autologin', TYPE_STR) === 'on';
+
+$result = array(
+    'error' => '',
+    'ok' => '',
+    'loginform' => ''
+);
 
 $err = 0;
-if (!empty($p_login) || !empty($p_pass)){
-    $err = $userManager->Login($p_login, $p_pass);
-    if ($err == 0){
-        header('Location: /');
+$isDoAuth = false;
+if (Abricos::$user->id === 0){
+    if (!empty($p_login) || !empty($p_password)){
+        $isDoAuth = true;
+        /** @var UserManager $manager */
+        $manager = Abricos::GetModuleManager('user');
+        $authManager = $manager->GetAuthManager();
+        $err = $authManager->Login($p_login, $p_password, $p_autologin);
+
+        if ($err){
+            $i18n = Abricos::GetModule('user')->I18n();
+
+            $result['auth'] = Brick::ReplaceVarByData($v['err'], array(
+                'err' => $i18n->Translate('content.login.error.'.$err)
+            ));
+        }
     }
 }
-if ($err == 0){
-    $brick->param->var['err'] = '';
-} else {
-    $brick->param->var['err'] = Brick::ReplaceVar($brick->param->var['err'], 'err', $brick->param->var['e'.$err]);
+
+if (Abricos::$user->id > 0 || ($isDoAuth && !$err)){
+    header('Location: /');
+    $result['auth'] = $v['ok'];
+} else if (!$isDoAuth || $err){
+    $brickLoginForm = Brick::$builder->LoadBrickS('user', 'loginform');
+    $result['loginform'] = Brick::ReplaceVarByData($v['form'], array(
+        'form' => $brickLoginForm->content
+    ));
 }
+
+$brick->content = Brick::ReplaceVarByData($brick->content, $result);
+
+
